c4b71038993131ac73a62fe5faa42812ec6d2146
ZhiLi0918 <zhili0918@gmail.com>
2024-12-26 17:25:13 -0500

    mongodb initial setup

--- backend /index.ts
+++ backend /index.ts
@@ -1,13 +1,36 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Application } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-const app = express();
-
+const app: Application = express();
 const port: number = 3000;
+
+// Cors
+app.use(cors())
+dotenv.config()
+
+// Parser
+app.use(express.json())
+app.use(
+   express.urlencoded({
+      extended: true
+   })
+)
 
 app.get('/', (req: Request, res: Response) => {
    res.send('Hello, TypeScript + Node.js + Express!');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
    console.log(`Server is running on http://localhost:${port}`);
+
+   // try{
+   //    await mongoose.connect(
+   //       process.env.MONGO_URL as string
+   //    )
+   //    console.log('connected to MongoDB')
+   // }catch(err){
+   //    console.log('Error connected to MongoDB', err)
+   // }
 });
