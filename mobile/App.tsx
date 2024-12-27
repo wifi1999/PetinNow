2e4a57fa3a6a9423743a791a5532055fde682b48
ZhiLi0918 <zhili0918@gmail.com>
2024-12-27 02:10:48 -0500

    setup register layout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Image, Text, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/Logo.png')} style={styles.logo} />
+
+      <View>
+        <View style={styles.row}>
+          <Text>Name</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setName}
+            value={name}
+          />
+        </View>
+        
+        <View style={styles.row}>
+          <Text>Email</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+          />
+        </View>
+
+        <View style={styles.row}>
+          <Text>Password</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setPassword}
+            value={password}
+          />
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +46,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D5B893',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 200,
+    height: 200
+  },
+  input: {
+    height: 25,
+    width: 100,
+    borderWidth: 1,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '10',
+    marginTop: 15
+  }
 });
